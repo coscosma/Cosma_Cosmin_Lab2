94c0b74c6963969c96ac998f467012ec7aef5b28
Facultate <Facultate@DESKTOP-BK3ITDP>
2023-10-22 19:32:41 +0300

    Add project files.

---
+++ Data/Cosma_Cosmin_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cosma_Cosmin_Lab2.Models;
+
+namespace Cosma_Cosmin_Lab2.Data
+{
+    public class Cosma_Cosmin_Lab2Context : DbContext
+    {
+        public Cosma_Cosmin_Lab2Context (DbContextOptions<Cosma_Cosmin_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cosma_Cosmin_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Cosma_Cosmin_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Cosma_Cosmin_Lab2.Data;
+using Cosma_Cosmin_Lab2.Models;
+
+namespace Cosma_Cosmin_Lab2.Pages.Publishers
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Cosma_Cosmin_Lab2.Data.Cosma_Cosmin_Lab2Context _context;
+
+        public DetailsModel(Cosma_Cosmin_Lab2.Data.Cosma_Cosmin_Lab2Context context)
+        {
+            _context = context;
+        }
+
+      public Publisher Publisher { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Publisher == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publisher.FirstOrDefaultAsync(m => m.ID == id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Publisher = publisher;
+            }
+            return Page();
+        }
+    }
+}
