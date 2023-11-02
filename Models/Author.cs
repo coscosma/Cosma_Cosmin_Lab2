8e23c50cf6ecc15e35d9cf00814d3637decf5690
coscosma <cos.cosma@gmail.com>
2023-11-02 20:09:11 +0200

    Laborator 4

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,4 +1,5 @@
 ﻿using Cosma_Cosmin_Lab2.Migrations;
+using System.ComponentModel.DataAnnotations;
 
 namespace Cosma_Cosmin_Lab2.Models
 {
@@ -8,6 +9,16 @@ namespace Cosma_Cosmin_Lab2.Models
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
 
         public ICollection<Book>? Books { get; set; }
     }
