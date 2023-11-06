fc0c8eb2a76d902e9227ef03982520bf3bfb2093
coscosma <cos.cosma@gmail.com>
2023-11-06 19:51:56 +0200

    Laborator5

--- Pages/Borrowings/Index.cshtml.cs
+++ Pages/Borrowings/Index.cshtml.cs
@@ -27,6 +27,8 @@ namespace Cosma_Cosmin_Lab2.Pages.Borrowings
             {
                 Borrowing = await _context.Borrowing
                 .Include(b => b.Book)
+                    .ThenInclude(b => b.Author)
+
                 .Include(b => b.Member).ToListAsync();
             }
         }
